2e87cac70af521390b586485f75e2944b86146db
conanguevon <conanguevon@guevon.com>
2022-03-10 17:11:54 -0500

    se agrega su cucha

---
+++ uno.js
@@ -0,0 +1,27 @@
+let etiquetaBoton=document.getElementById("boton")
+etiquetaBoton.addEventListener("click",escuchar)
+function escuchar(event) {
+    event.preventDefault()
+    console.log("hizo click")
+
+    let empanada=document.getElementById("empanada").value
+    console.log (empanada)
+
+    let cedula=document.getElementById("cedula").value
+    console.log (cedula)
+
+    let nombre=document.getElementById("nombre").value
+    console.log (nombre)
+
+    let correo=document.getElementById("correo").value
+    console.log (correo)
+
+}
+
+
+
+
+
+
+
+
